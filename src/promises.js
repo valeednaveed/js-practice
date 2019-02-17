5130c23296991afc89a82f35dd8d68d65fe8c6d5
Waleed Naveed <valeednaveed@gmail.com>
2019-02-17 13:53:03 +0500

    Promises concepts

---
+++ src/promises.js
@@ -0,0 +1,44 @@
+console.log("Bismillah"); 
+
+let successPromise = new Promise((resolve,reject)=> {
+    resolve('success');  
+
+}); 
+
+let successPromise2 = new Promise((resolve,reject)=> {
+    resolve('success 2');  
+
+}); 
+
+let errorPromise = new Promise((resolve,reject)=> {
+    reject('error');  
+
+}); 
+
+
+let errorPromise2 = new Promise((resolve,reject)=> {
+    reject('error 2');  
+
+}); 
+
+
+successPromise.then((res)=>{
+    console.log(res); 
+}).catch((err)=>{
+console.log(err); 
+});
+
+
+errorPromise.then((res)=>{
+    console.log(res); 
+}).catch((err)=>{
+console.log(err); 
+});
+
+Promise.all([successPromise,successPromise2]).then((data)=>{
+console.log(data); 
+})
+
+Promise.all([errorPromise,errorPromise2]).catch((err)=>{
+    console.log(err); 
+    })
